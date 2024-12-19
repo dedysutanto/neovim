49915bf26d66d412feda37a724bdabe005109470
proitlab <dedy.sutanto@proit.co.id>
2024-12-19 10:52:23 +0700

    Install Rust support

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -47,4 +47,11 @@ return {
       },
     },
   },
+
+  -- Rust Tool replacement
+  {
+    "mrcjkb/rustaceanvim",
+    version = "^5", -- Recommended
+    lazy = false, -- This plugin is already lazy
+  },
 }
